4514c831bf51ca6beaa95ca6810bc88fcf4b5a00
Davies Ningthouja <meiteidavies@gmail.com>
2024-12-28 23:56:08 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Hero from './component/Hero.jsx'
+import About from './component/About.jsx'
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
     <Hero/> 
-    <section className='z-0 min-h-screen bg-blue-500'/>
+    <About/>
+    
+    {/* className='z-0 min-h-screen bg-blue-500'/> */}
     </main>
   )
 }
