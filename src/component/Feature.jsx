b5a01aadcb4954ab3a19b5457f17660d66de2a0f
Davies Ningthouja <meiteidavies@gmail.com>
2025-01-01 03:08:56 +0530

    Feature Update

--- src/component/Feature.jsx
+++ src/component/Feature.jsx
@@ -1,6 +1,61 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, { useRef, useState } from 'react'
 
-const BentoCard = ({src, title, description, isComingSoon }) => {
+import {  TiLocationArrow } from 'react-icons/ti'
+//Tilt Animation
+const BentoTilt = ({children , className = ''}) =>{
+  const [transformStyle, setTransformStyle] = useState('');
+  const [isGrabbing, setIsGrabbing] = useState(false);
+  const itemRef = useRef(null);
+
+  const handleMouseMove = (e) => {
+    if(!itemRef.current) return;
+    
+    const {left, top ,width , height } = itemRef.current.getBoundingClientRect()
+
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top ) / height;
+
+    const tiltX = (relativeY - 0.5) * 7;
+    const tiltY = (relativeX - 0.5) * -7;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d( 0.92, 0.92, 0.92)`;
+    
+
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformStyle('')
+  };
+
+  const handleMouseDown = () => {
+    setIsGrabbing(true); // Change to grabbing cursor
+  };
+
+  const handleMouseUp = () => {
+    setIsGrabbing(false); // Change back to grab cursor
+  };
+  return(
+    <div 
+      ref={itemRef}
+      className={`${className}  ${isGrabbing ? 'cursor-grabbing' : 'cursor-grab'}`}
+      onMouseMove={handleMouseMove} 
+      onMouseLeave={handleMouseLeave}
+       onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      style={{
+        transform: transformStyle,
+        transition: 'transform 0.4s ease-linear', // Smooth transition
+        willChange: 'transform', // Optimize performance
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+const BentoCard = ({src, title, description,  con }) => {
   return(
     <div className='relative size-full'>
       <video
@@ -8,7 +63,7 @@ const BentoCard = ({src, title, description, isComingSoon }) => {
         loop
         muted
         autoPlay
-        className='absolute left-0 top-0 size-full object-cover'
+        className={con ? con : 'absolute left-0 top-0 size-full object-cover'}
       />
       <div className='relative z-10 flex size-full flex-col justify-between p-5 text-blue-50'>
         <div>
@@ -36,45 +91,62 @@ const Feature = () => {
 
         {/* bento 1 */}
         {/* h-96 */}
-        <div className='border-hsla relative mb-7  h-[25rem] w-full overflow-hidden rounded-md md:h-[65vh]'>
+        <BentoTilt className='border-hsla relative mb-7  h-[25rem] w-full overflow-hidden rounded-md md:h-[65vh]'>
           <BentoCard
             src="videos/feature-1.mp4"
             title={<>radie<b>n</b>t</>}
             description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
             isComingSoon ={true}
             />
-        </div>
+        </BentoTilt>
         {/* gap-7 */}
-        <div className='grid h-[135vh] grid-cols-2 grid-rows-3 gap-5'>
+        <div className='grid h-[140vh] grid-cols-2 grid-rows-3 gap-4'>
           {/* bento 2 */}
-          <div className='bento-tilt_1 row-span-2 md:col-span-1 md:row-span-2'>
+          <BentoTilt className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2'>
             <BentoCard
               src="videos/feature-2.mp4"
               title={<>zig<b>m</b>a</>}
               description={'An anime and gaming-inspired NFT collection - the IP primed for expansion.'}
             />
 
-          </div>
+          </BentoTilt>
           {/* bento 3 */}
-          <div className='bento-tilt_1 row-span-1 me-14'>
+          <BentoTilt className='bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0'>
             <BentoCard
               src="videos/feature-3.mp4"
               title={<>n<b>e</b>xus</>}
+              con="absolute left-[6rem] top-0 size-full object-cover"
               description={"A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."}
             />
-          </div>
+          </BentoTilt>
           {/* bento 4 */}
-          <div className='bento-tilt_2 row-span-1 ms-32 md:col-span-1 md:ms-0'>
+          <BentoTilt className='bento-tilt_1 me-14 sm:col-span-2 md:col-span-1 md:me-0'>
             <BentoCard
               src="videos/feature-4.mp4"
               title={<>as<b>u</b>l</>}
               description={"A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."}
             />
-          </div>
+          </BentoTilt>
 
+          <BentoTilt className='bento-tilt_2' >
+            <div className='flex size-full flex-col justify-between bg-violet-300 p-5'>
+              <h1 className='bento-title special-font max-w-64 text-black'>M<b>o</b>re co<b>m</b>ing s<b>o</b>o<b>n</b>!</h1>
+              <TiLocationArrow className='arrowIcon scale-[5]'/>
+            </div>
+          </BentoTilt>
+
+          <BentoTilt className='bento-tilt_2'>
+            <video
+              src='/videos/feature-5.mp4'
+              loop
+              muted
+              autoPlay
+              className='size-full object-cover object-center'
+            />
+          </BentoTilt>
         </div>
       </div>
-      
+      <section className='m-72 h-[145dvh]'></section>
     </section>
   )
 }
